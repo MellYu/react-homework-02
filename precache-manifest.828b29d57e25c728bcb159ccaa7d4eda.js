e64f598af19397cf127bf2b34aaf5c423ffe5490
MellYu <mel6273569@gmail.com>
2020-10-04 12:10:43 +0300

    Updates

---
+++ precache-manifest.828b29d57e25c728bcb159ccaa7d4eda.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db8e128df570f5fca87417451200881a",
+    "url": "/react-homework-02/index.html"
+  },
+  {
+    "revision": "51129484d8b44a2886fa",
+    "url": "/react-homework-02/static/js/2.30ea518a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-homework-02/static/js/2.30ea518a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37dda511d1ccb033cb04",
+    "url": "/react-homework-02/static/js/main.13eedbbb.chunk.js"
+  },
+  {
+    "revision": "826fadd45d99892e2909",
+    "url": "/react-homework-02/static/js/runtime-main.9377813b.js"
+  }
+]);
